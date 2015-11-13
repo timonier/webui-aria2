ff00618a822b695e9c61ef5238603936ca2bf0de
Morgan AUCHEDE <morgan.auchede@gmail.com>
2015-11-13 12:22:25 +0100

    Remove default token.

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 RPC_HOST=${RPC_HOST:-127.0.0.1}
 RPC_PORT=${RPC_PORT:-6800}
-RPC_SECRET=${RPC_SECRET:-00000000-0000-0000-0000-000000000000}
+RPC_SECRET=${RPC_SECRET:-}
 
 sed -i "s@%RPC_HOST%@$RPC_HOST@g" /var/www/webui-aria2/configuration.js
 sed -i "s@%RPC_PORT%@$RPC_PORT@g" /var/www/webui-aria2/configuration.js
